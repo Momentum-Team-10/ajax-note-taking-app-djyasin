d3b0ee407322f40239606058e061d2589eb96e44
Lila <djyasin.ly@gmail.com>
2021-10-15 16:59:41 -0400

    Somehow created an inifinite loop when fixing other bugs. Will revisit tomorrow.

--- script.js
+++ script.js
@@ -21,68 +21,76 @@ root.innerHTML = `
  </div>
 `;
 
-
-const aideMemoire = document.getElementById('aideMemoire')
-const inputBox = document.querySelector('#inputBox')
-const listofNotes= document.getElementById('listofNotes')
-const notesBody = document.getElementById('notesBody')
+const aideMemoire = document.getElementById("aideMemoire");
+const inputBox = document.querySelector("#inputBox");
+const listofNotes = document.getElementById("listofNotes");
+const notesBody = document.getElementById("notesBody");
 //const inputBox = document.getElementById('inputBox')
 //this is to grab the list from the DOM
 
-notesBody.addEventListener('submit', (e) => {
-e.preventDefault()
-const inputBox = document.getElementById('inputBox').value
-console.log(inputBox)
-
-createNote(inputBox)
-// Clear form after a todo has been created
-form.reset()
+inputBox.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const inputBox = document.getElementById("inputBox").value;
+  console.log(inputBox);
+  createNote(inputBox);
 })
 
-notesBody.addEventListener('click', (e) => { 
-    let inputBox = document.getElementById('inputBox')
-    if (e.target.classlist.contains('delete')) {
-        createNote(inputBox)
-        form.reset()
-    }
-
+notesBody.addEventListener("click", (e) => {
+  let inputBox = document.getElementById("inputBox");
+  if (e.target.classlist.contains("delete")) {
+    createNote(inputBox);
+    form.reset();
+  }
 })
-
 
 function noteList() {
-    fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-            for (let item of data) {
-                noteLines(item)
-            }
-        })
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      for (let item of data) {
+        renderListOfNotes(item);
+      }
+    });
 }
-function renderTodoItem(noteObj) {
-    const li = document.createElement('li')
-    li.id = noteObj.id
-    li.classList.add()
-    renderTodoText(li, todoObj)
-    todoList.appendChild(li)
+function renderListOfNotes(noteObj) {
+  let li = document.createElement("li");
+  li.id = noteObj.id;
+  li.classList.add();
+  renderListOfNotes(li, noteObj);
+  listofNotes.appendChild(li);
 }
 
 function createNote(noteText) {
-    fetch(url,{
-        method: 'POST',
-        headers: {'Content-Type': 'application/json', Authorization: 'Token '},
-        body: JSON.stringify({
-            title: noteText,
-            body: noteText,
-            created_at: moment().format()
-        })
-    })
-    .then(res => res.json())
-    .then(data => renderTodoItem(data))
+  fetch(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/json", Authorization: "Token " },
+    body: JSON.stringify({
+      title: noteText,
+      body: noteText,
+      created_at: moment().format(),
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => renderListOfNotes(data));
 }
 
-function deleteNote(listofNotesEl) {
-    fetch(url + '/' `$(listOfNotesEl.id)`, {
-    method: 'DELETE'
-    }).then(() => listofNotesEl.ParentElement.remove())
-}
+//function deleteNote(listofNotesEl) {
+//  fetch(url + "/"`$(listOfNotesEl.id)`, {
+//   method: "DELETE",
+//  }).then(() => listofNotesEl.ParentElement.remove());
+//}
+//function updateNote(noteEl)
+//  let noteText = document.getElementById('note-text').value
+//   fetch(url + '/' `${noteEl.id}`, {
+//      method: 'PUT',
+//      headers: { 'Content-Type': 'application/json' },
+//       body: JSON.stringify({
+//           title: noteText,
+//           body: noteText,
+//           updated_at: moment().format()
+//       })
+//   }
+//   )
+
+noteList();
